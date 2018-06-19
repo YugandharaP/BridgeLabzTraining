ed8aefbdba2ec700de9507bd52ef5a67952420c3
Chaitra111 <111chaitra@gmail>
2018-06-19 19:49:52 +0530

    utility

---
+++ FunctionalPrograms/src/com/bridgelabz/utility/UtilityForAlgorithms.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.utility;
+
+import java.util.Arrays;
+
+public class UtilityForAlgorithms {
+
+
+}
+
+	
+	
+	/*public static <T extends Comparable<T>> T genericMax(T first,T second,T third)
+	{
+		if(first.compareTo(second)>0 && first.compareTo(third)>0 )
+			return first;
+		else if(second.compareTo(first)>0 && second.compareTo(third)>0)
+			return second;
+		else
+			return third;
+	}
+	
+	public static int integerMax(int first,int second,int third)
+	{
+		return (first>second && first>third) ? first : (second>first && second>third) ? second : third ;  
+	}
+	
+	
+	public static String stringMax(String first,String second,String third)
+	{
+		return (first.compareTo(second)>0 && first.compareTo(third)>0) ? first : (second.compareTo(first) >0 && second.compareTo(third)> 0) ? second : third ; 
+	}*/
+
